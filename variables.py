e3d61c8fea91887270959c646590a2105e5cdafe
GongMeiting2020 <Meiting.19@intl.zju.edu.cn>
2020-05-13 12:28:07 +0800

    add helloword.py and variables.py

---
+++ variables.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 20 00:26:39 2020
+
+@author: gongmeiting
+"""
+a=457
+b=a*1000+a
+print(b%7==0)
+c=b/7
+d=c/11
+e=d/13
+print(a==e)
+print(a>e)
+print(a<e)
+#a==e is always True since b/a=7*11*13
+
+#another code, to avoid same variables,use f~j to represnt a~e
+f=input ("a three-digit number:")
+g=int(f)*1000+int(f)
+if g%7==0:
+    h=g/7
+    i=h/11
+    j=i/13
+    if int (f)>j:
+        print("f>j")
+    elif int (f)<j:
+        print("f<j")
+    else:
+        print("f=j")
+else:
+    print("g cannot devided by 7")
+
+
+X=input
+Y=input
+k=X and not Y
+l=Y and not X
+Z=k or l
+W= X!=Y
+print(Z=W)
